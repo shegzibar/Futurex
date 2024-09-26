e77bd3d587561c09c94d602e58da05c0374a9a1c
segzibar <shegzibar@gmail.com>
2024-09-27 02:17:18 +0300

    added the new assistance page

---
+++ lib/screens/bot_screens/assistance_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class assistance extends StatefulWidget {
+  const assistance({super.key});
+
+  @override
+  State<assistance> createState() => _assistanceState();
+}
+
+class _assistanceState extends State<assistance> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
