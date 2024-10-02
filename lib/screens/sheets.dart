2b0da3c51a116a029155b01202f9e0bd005f1dbc
segzibar <shegzibar@gmail.com>
2024-10-02 12:51:31 +0300

    adding the new dynamic sheet page which is going to be triggered by the chatbot

---
+++ lib/screens/sheets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class sheet extends StatefulWidget {
+  const sheet({super.key});
+
+  @override
+  State<sheet> createState() => _sheetState();
+}
+
+class _sheetState extends State<sheet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
